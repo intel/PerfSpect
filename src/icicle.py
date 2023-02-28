140e97084e2362959e80e0f438ea98e08bff7843
Daniel Hill <daniel.hill@intel.com>
2023-02-28 15:57:50 -0800

    add python3 header to all python files

--- src/icicle.py
+++ src/icicle.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 ###########################################################################################################
 # Copyright (C) 2020-2023 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
