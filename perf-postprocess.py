b625033115c8cb5258de813c7e666c12bece324e
Harper, Jason M <jason.m.harper@intel.com>
2024-08-28 10:01:48 -0700

    fix parse of cgroup cpusets

--- perf-postprocess.py
+++ perf-postprocess.py
@@ -254,6 +254,23 @@ def get_all_data_lines(input_file_path):
     return meta_data_lines, perf_events_lines, perf_data_lines
 
 
+# return the number of cpus in a set
+# example sets:
+#   simple range:  "0-95"
+#   individual cpus: "1+4+5+9"
+#   individual cpus and range(s): "1+4-7+9+12-14"
+def count_cpus_in_set(cpu_set):
+    count = 0
+    subsets = cpu_set.split("+")
+    for subset in subsets:
+        if "-" in subset:
+            low_high = subset.split("-")
+            count += int(low_high[1]) - int(low_high[0]) + 1
+        else:
+            count += 1
+    return count
+
+
 # get_metadata
 def get_metadata_as_dict(meta_data_lines, txns=None):
     meta_data = {}
@@ -312,14 +329,7 @@ def get_metadata_as_dict(meta_data_lines, txns=None):
             assert len(docker_HASH) == len(docker_SETS)
             meta_data["CPUSETS"] = {}
             for i, docker_SET in enumerate(docker_SETS):
-                if "-" in docker_SET:  # range of cpus
-                    num_of_cpus = (
-                        int(docker_SET.split("-")[1])
-                        - int(docker_SET.split("-")[0])
-                        + 1
-                    )
-                else:  # either one cpu, or a list of cpus separated by + sign
-                    num_of_cpus = len(docker_SET.split("+"))
+                num_of_cpus = count_cpus_in_set(docker_SET)
                 meta_data["CPUSETS"][docker_HASH[i]] = num_of_cpus
 
         elif line.startswith("Percpu mode"):
@@ -844,11 +854,11 @@ def substitute_event_in_expression(
         if event_df.shape == (1,):  # system wide
             if "sys" not in evaluated_expressions:
                 evaluated_expressions["sys"] = exp_to_evaluate.replace(
-                    "[" + event + "]", str(event_df[0])
+                    "[" + event + "]", str(event_df.iloc[0])
                 )
             else:
                 evaluated_expressions["sys"] = evaluated_expressions["sys"].replace(
-                    "[" + event + "]", str(event_df[0])
+                    "[" + event + "]", str(event_df.iloc[0])
                 )
         else:
             for index in event_df.index:
