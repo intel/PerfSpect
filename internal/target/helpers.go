8f781cd928e997352a8190783e8ba43c5a439e5b
Harper, Jason M <jason.m.harper@intel.com>
2025-04-26 13:30:13 -0700

    refactor target package
    Signed-off-by: Harper, Jason M <jason.m.harper@intel.com>

---
+++ internal/target/helpers.go
@@ -0,0 +1,237 @@
+package target
+
+// Copyright (C) 2021-2025 Intel Corporation
+// SPDX-License-Identifier: BSD-3-Clause
+
+import (
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+// installLkms attempts to install a list of Linux Kernel Modules (LKMs) on the target system.
+// It requires elevated privileges to perform the installation.
+//
+// Parameters:
+//   - t: A Target interface that represents the system where the LKMs will be installed.
+//     The Target must support privilege elevation.
+//   - lkms: A slice of strings representing the names of the LKMs to be installed.
+//
+// Returns:
+//   - installedLkms: A slice of strings containing the names of the LKMs that were successfully installed.
+//   - err: An error if privilege elevation is not possible or if any other issue occurs.
+func installLkms(t Target, lkms []string) (installedLkms []string, err error) {
+	if !t.CanElevatePrivileges() {
+		err = fmt.Errorf("can't elevate privileges; elevated privileges required to install lkms")
+		return
+	}
+	for _, lkm := range lkms {
+		slog.Debug("attempting to install kernel module", slog.String("lkm", lkm))
+		_, _, _, err := t.RunCommand(exec.Command("modprobe", "--first-time", lkm), 10, true)
+		if err != nil {
+			slog.Debug("kernel module already installed or problem installing", slog.String("lkm", lkm), slog.String("error", err.Error()))
+			continue
+		}
+		slog.Debug("kernel module installed", slog.String("lkm", lkm))
+		installedLkms = append(installedLkms, lkm)
+	}
+	return
+}
+
+// uninstallLkms attempts to uninstall a list of Linux kernel modules (LKMs) from the target system.
+// It requires elevated privileges to perform the operation.
+//
+// Parameters:
+//   - t: A Target interface that represents the system where the LKMs will be uninstalled.
+//     The Target must support privilege elevation.
+//   - lkms: A slice of strings representing the names of the kernel modules to be uninstalled.
+//
+// Returns:
+//   - err: An error if privilege elevation is not possible or if any other issue occurs during the process.
+func uninstallLkms(t Target, lkms []string) (err error) {
+	if !t.CanElevatePrivileges() {
+		err = fmt.Errorf("can't elevate privileges; elevated privileges required to uninstall lkms")
+		return
+	}
+	for _, lkm := range lkms {
+		slog.Debug("attempting to uninstall kernel module", slog.String("lkm", lkm))
+		_, _, _, err := t.RunCommand(exec.Command("modprobe", "-r", lkm), 10, true)
+		if err != nil {
+			slog.Error("error uninstalling kernel module", slog.String("lkm", lkm), slog.String("error", err.Error()))
+			continue
+		}
+		slog.Debug("kernel module uninstalled", slog.String("lkm", lkm))
+	}
+	return
+}
+
+// runLocalCommandWithInputWithTimeout executes a local command with optional input and a timeout.
+// It captures the command's standard output, standard error, and exit code.
+//
+// Parameters:
+//   - cmd: The command to execute, represented as an *exec.Cmd.
+//   - input: A string to be passed as input to the command's standard input.
+//   - timeout: The timeout in seconds for the command execution. If set to 0, no timeout is applied.
+//
+// Returns:
+//   - stdout: The standard output of the command as a string.
+//   - stderr: The standard error of the command as a string.
+//   - exitCode: The exit code of the command. If the command fails to execute, this may be undefined.
+//   - err: An error object if the command fails to execute or times out.
+func runLocalCommandWithInputWithTimeout(cmd *exec.Cmd, input string, timeout int) (stdout string, stderr string, exitCode int, err error) {
+	logInput := ""
+	if input != "" {
+		logInput = "******"
+	}
+	slog.Debug("running local command", slog.String("cmd", cmd.String()), slog.String("input", logInput), slog.Int("timeout", timeout))
+	if timeout > 0 {
+		var cancel context.CancelFunc
+		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
+		defer cancel()
+		commandWithContext := exec.CommandContext(ctx, cmd.Path, cmd.Args[1:]...) // nosemgrep
+		commandWithContext.Env = cmd.Env
+		cmd = commandWithContext
+	}
+	if input != "" {
+		cmd.Stdin = strings.NewReader(input)
+	}
+	var outbuf, errbuf strings.Builder
+	cmd.Stdout = &outbuf
+	cmd.Stderr = &errbuf
+	err = cmd.Run()
+	stdout = outbuf.String()
+	stderr = errbuf.String()
+	if err != nil {
+		exitError := &exec.ExitError{}
+		if errors.As(err, &exitError) {
+			exitCode = exitError.ExitCode()
+		}
+	}
+	return
+}
+
+// runLocalCommandWithInputWithTimeoutAsync executes a local command asynchronously with optional input and timeout.
+// It streams the command's stdout and stderr to the provided channels and sends the exit code to the exitcodeChannel.
+//
+// Parameters:
+//   - cmd: The command to execute, represented as an *exec.Cmd.
+//   - stdoutChannel: A channel to send lines of stdout output.
+//   - stderrChannel: A channel to send lines of stderr output.
+//   - exitcodeChannel: A channel to send the exit code of the command.
+//   - input: A string to be passed as input to the command's stdin. If empty, no input is provided.
+//   - timeout: The timeout in seconds for the command execution. If 0 or less, no timeout is applied.
+//
+// Returns:
+//   - err: An error if the command fails to start or if there are issues with pipes.
+func runLocalCommandWithInputWithTimeoutAsync(cmd *exec.Cmd, stdoutChannel chan string, stderrChannel chan string, exitcodeChannel chan int, input string, timeout int) (err error) {
+	logInput := ""
+	if input != "" {
+		logInput = "******"
+	}
+	slog.Debug("running local command (async)", slog.String("cmd", cmd.String()), slog.String("input", logInput), slog.Int("timeout", timeout))
+	if timeout > 0 {
+		var cancel context.CancelFunc
+		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
+		defer cancel()
+		commandWithContext := exec.CommandContext(ctx, cmd.Path, cmd.Args[1:]...) // nosemgrep
+		commandWithContext.Env = cmd.Env
+		cmd = commandWithContext
+	}
+	if input != "" {
+		cmd.Stdin = strings.NewReader(input)
+	}
+	stdoutReader, err := cmd.StdoutPipe()
+	if err != nil {
+		err = fmt.Errorf("failed to get stdout pipe: %v", err)
+		return
+	}
+	stdoutScanner := bufio.NewScanner(stdoutReader)
+	stderrReader, err := cmd.StderrPipe()
+	if err != nil {
+		err = fmt.Errorf("failed to get stderr pipe: %v", err)
+		return
+	}
+	stderrScanner := bufio.NewScanner(stderrReader)
+	if err = cmd.Start(); err != nil {
+		err = fmt.Errorf("failed to run command (%s): %v", cmd, err)
+		return
+	}
+	go func() {
+		for stdoutScanner.Scan() {
+			text := stdoutScanner.Text()
+			stdoutChannel <- text
+		}
+	}()
+	go func() {
+		for stderrScanner.Scan() {
+			text := stderrScanner.Text()
+			stderrChannel <- text
+		}
+	}()
+	err = cmd.Wait()
+	if err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			exitcodeChannel <- exitError.ExitCode()
+		} else {
+			panic(fmt.Sprintf("err from cmd.Wait is not type exec.ExitError: %v", err))
+		}
+	} else {
+		exitcodeChannel <- 0
+	}
+	return nil
+}
+
+func getArchitecture(t Target) (arch string, err error) {
+	cmd := exec.Command("uname", "-m")
+	arch, _, _, err = t.RunCommand(cmd, 0, true)
+	if err != nil {
+		return
+	}
+	arch = strings.TrimSpace(arch)
+	return
+}
+
+func getFamily(t Target) (family string, err error) {
+	cmd := exec.Command("bash", "-c", "lscpu | grep -i \"^CPU family:\" | awk '{print $NF}'")
+	family, _, _, err = t.RunCommand(cmd, 0, true)
+	if err != nil {
+		return
+	}
+	family = strings.TrimSpace(family)
+	return
+}
+
+func getModel(t Target) (model string, err error) {
+	cmd := exec.Command("bash", "-c", "lscpu | grep -i model: | awk '{print $NF}'")
+	model, _, _, err = t.RunCommand(cmd, 0, true)
+	if err != nil {
+		return
+	}
+	model = strings.TrimSpace(model)
+	return
+}
+
+func getStepping(t Target) (stepping string, err error) {
+	cmd := exec.Command("bash", "-c", "lscpu | grep -i stepping: | awk '{print $NF}'")
+	stepping, _, _, err = t.RunCommand(cmd, 0, true)
+	if err != nil {
+		return
+	}
+	stepping = strings.TrimSpace(stepping)
+	return
+}
+
+func getVendor(t Target) (vendor string, err error) {
+	cmd := exec.Command("bash", "-c", "lscpu | grep -i \"^Vendor ID:\" | awk '{print $NF}'")
+	vendor, _, _, err = t.RunCommand(cmd, 0, true)
+	if err != nil {
+		return
+	}
+	vendor = strings.TrimSpace(vendor)
+	return
+}
