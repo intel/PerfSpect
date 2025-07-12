76bf520db9036fe893f73fe48c7d9ee3ba68d8c7
Harper, Jason M <jason.m.harper@intel.com>
2025-07-12 13:55:40 -0700

    refactor metric and event loading
    Signed-off-by: Harper, Jason M <jason.m.harper@intel.com>

---
+++ cmd/metrics/loader_dynamic.go
@@ -0,0 +1,5 @@
+package metrics
+
+func (l *DynamicLoader) Load(metricDefinitionOverridePath string, eventDefinitionOverridePath string, selectedMetrics []string, metadata Metadata) ([]MetricDefinition, []GroupDefinition, error) {
+	return nil, nil, nil
+}
