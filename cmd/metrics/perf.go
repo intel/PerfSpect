bec83ee75e3c2de4e3f430f631b022391f0dc235
Gubrud, Aaron D <aaron.d.gubrud@intel.com>
2025-08-01 15:21:12 -0700

    first pass at adding filtered core focus

--- cmd/metrics/perf.go
+++ cmd/metrics/perf.go
@@ -94,10 +94,13 @@ func getPerfPath(myTarget target.Target, localPerfPath string) (string, error) {
 // Returns:
 // - args: The command arguments for the 'perf stat' command.
 // - err: An error, if any.
-func getPerfCommandArgs(pids []string, cgroups []string, timeout int, eventGroups []GroupDefinition) (args []string, err error) {
+func getPerfCommandArgs(pids []string, cgroups []string, timeout int, eventGroups []GroupDefinition, coreRange string) (args []string, err error) {
 	// -I: print interval in ms
 	// -j: json formatted event output
 	args = append(args, "stat", "-I", fmt.Sprintf("%d", flagPerfPrintInterval*1000), "-j")
+	if coreRange != "" {
+		args = append(args, "-C", coreRange) // collect only for these cores
+	}
 	switch flagScope {
 	case scopeSystem:
 		args = append(args, "-a") // system-wide collection
@@ -133,7 +136,7 @@ func getPerfCommandArgs(pids []string, cgroups []string, timeout int, eventGroup
 
 // getPerfCommand is responsible for assembling the command that will be
 // executed to collect event data
-func getPerfCommand(perfPath string, eventGroups []GroupDefinition, pids []string, cids []string) (*exec.Cmd, error) {
+func getPerfCommand(perfPath string, eventGroups []GroupDefinition, pids []string, cids []string, coreRange string) (*exec.Cmd, error) {
 	var duration int
 	switch flagScope {
 	case scopeSystem:
@@ -148,7 +151,7 @@ func getPerfCommand(perfPath string, eventGroups []GroupDefinition, pids []strin
 		duration = 0
 	}
 
-	args, err := getPerfCommandArgs(pids, cids, duration, eventGroups)
+	args, err := getPerfCommandArgs(pids, cids, duration, eventGroups, coreRange)
 	if err != nil {
 		err = fmt.Errorf("failed to assemble perf args: %v", err)
 		return nil, err
