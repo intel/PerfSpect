f1435e04057503e8b63522cbb2924b7ffa7d534a
echiugoog <edwinchiu@google.com>
2025-11-27 11:17:52 -0500

    Enhance fio benchmark to measure latency, IOPs and bandwidth (#551)
    * Switch fio benchmark to use libaio and multiple fiojobs to measure IOPs
    and throughput. There is an increase in disk space needed to run now,
    from 5G to 16G.
    
    * Add in libaio-aarch64 and update fio-aarch64 to support libaio on arm
    
    * remove overly verbose/long debug of fio raw output
    
    * check for storage benchmark errors earlier to catch conditions like
    insufficient diskspace
    
    log partial storage benchmark output if unable to find fio output
    
    * Add back in diskspace check in storage benchmark, space based on
    bandwidth test which uses the most diskspace
    
    Add back in sync and dropping vm cache
    
    * modernize
    
    ---------
    
    Co-authored-by: Harper, Jason M <jason.m.harper@intel.com>

--- internal/report/table_defs.go
+++ internal/report/table_defs.go
@@ -669,7 +669,7 @@ var tableDefinitions = map[string]TableDefinition{
 	StorageBenchmarkTableName: {
 		Name:      StorageBenchmarkTableName,
 		MenuLabel: StorageBenchmarkTableName,
-		HasRows:   false,
+		HasRows:   true,
 		ScriptNames: []string{
 			script.StorageBenchmarkScriptName,
 		},
@@ -2440,15 +2440,50 @@ func numaBenchmarkTableValues(outputs map[string]script.ScriptOutput) []Field {
 	return fields
 }
 
+// formatOrEmpty formats a value and returns an empty string if the formatted value is "0".
+func formatOrEmpty(format string, value any) string {
+	s := fmt.Sprintf(format, value)
+	if s == "0" {
+		return ""
+	}
+	return s
+}
+
 func storageBenchmarkTableValues(outputs map[string]script.ScriptOutput) []Field {
-	readBW, writeBW := storagePerfFromOutput(outputs)
-	if readBW == "" && writeBW == "" {
+	fioData, err := storagePerfFromOutput(outputs)
+	if err != nil {
+		slog.Error("failed to get storage benchmark data", slog.String("error", err.Error()))
 		return []Field{}
 	}
-	return []Field{
-		{Name: "Single-Thread Read Bandwidth", Values: []string{readBW}},
-		{Name: "Single-Thread Write Bandwidth", Values: []string{writeBW}},
+
+	if len(fioData.Jobs) == 0 {
+		return []Field{}
 	}
+
+	// Initialize the fields for metrics (column headers)
+	fields := []Field{
+		{Name: "Job"},
+		{Name: "Read Latency (us)"},
+		{Name: "Read IOPs"},
+		{Name: "Read Bandwidth (MiB/s)"},
+		{Name: "Write Latency (us)"},
+		{Name: "Write IOPs"},
+		{Name: "Write Bandwidth (MiB/s)"},
+	}
+
+	// For each FIO job, create a new row and populate its values
+	slog.Debug("fioData", slog.Any("jobs", fioData.Jobs))
+	for _, job := range fioData.Jobs {
+		fields[0].Values = append(fields[0].Values, job.Jobname)
+		fields[1].Values = append(fields[1].Values, formatOrEmpty("%.0f", job.Read.LatNs.Mean/1000))
+		fields[2].Values = append(fields[2].Values, formatOrEmpty("%.0f", job.Read.IopsMean))
+		fields[3].Values = append(fields[3].Values, formatOrEmpty("%d", job.Read.Bw/1024))
+		fields[4].Values = append(fields[4].Values, formatOrEmpty("%.0f", job.Write.LatNs.Mean/1000))
+		fields[5].Values = append(fields[5].Values, formatOrEmpty("%.0f", job.Write.IopsMean))
+		fields[6].Values = append(fields[6].Values, formatOrEmpty("%d", job.Write.Bw/1024))
+	}
+
+	return fields
 }
 
 // telemetry
