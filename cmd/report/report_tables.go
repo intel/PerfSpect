ade5c636dafed76aafe24a8c58425b1576954295
Harper, Jason M <jason.m.harper@intel.com>
2025-12-15 03:43:06 -0800

    formatting
    Signed-off-by: Harper, Jason M <jason.m.harper@intel.com>

--- cmd/report/report_tables.go
+++ cmd/report/report_tables.go
@@ -1808,4 +1808,3 @@ func dimmTableHTMLRenderer(tableValues table.TableValues, targetName string) str
 	}
 	return report.RenderHTMLTable(socketTableHeaders, socketTableValues, "pure-table pure-table-bordered", [][]string{})
 }
-
